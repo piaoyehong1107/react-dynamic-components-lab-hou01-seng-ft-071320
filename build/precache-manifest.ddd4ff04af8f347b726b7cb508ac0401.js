0d042bf8d7a6d392e880f3a0d6cb9191c801d54e
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-14 21:35:28 +0000

    Automatically backed up by Learn

---
+++ build/precache-manifest.ddd4ff04af8f347b726b7cb508ac0401.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1557044b3b6bafd396a0",
+    "url": "/static/js/main.6a193fd8.chunk.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "1557044b3b6bafd396a0",
+    "url": "/static/css/main.e4845517.chunk.css"
+  },
+  {
+    "revision": "5fc737806de4cd77cd48127b372bbd44",
+    "url": "/index.html"
+  }
+];
